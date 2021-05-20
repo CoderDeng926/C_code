01eb38fd571053f42490872231c827ac072fdeb5
CoderDeng926 <djq2926@icloud.com>
2021-05-20 19:17:46 +0800

    21年5月20日 牛客网 编程初学者入门训练 DAY7

---
+++ Project_21_5_20/practice.c
@@ -0,0 +1,121 @@
+#include <stdio.h>
+
+int main()
+{
+	//牛客网 编程初学者入门训练
+	//DAY7
+
+	//BC59
+	//int num = 0;
+	//while (scanf_s("%d", &num) != EOF)
+	//{
+	//	int i = 0, j = 0;
+	//	for (i = 0; i < num; i++)
+	//	{
+	//		printf("* ");
+	//		for (j = 0; j < num - 1 - i; j++)
+	//		{
+	//			printf("* ");
+	//		}
+	//		printf("\n");
+	//	}
+	//}
+
+	//BC60
+	//int num = 0;
+	//int i = 0, j = 0;
+	//while (scanf_s("%d", &num) != EOF)
+	//{
+	//	for (i = 0; i < num; i++)
+	//	{
+	//		
+	//		for (j = 0; j < num - 1 - i; j++)
+	//		{
+	//			printf("  ");
+	//		}
+	//		for (j = 0; j < i; j++)
+	//		{
+	//			printf("* ");
+	//		}
+	//		printf("* ");
+	//		printf("\n");
+	//	}
+	//}
+
+	//BC61
+	//int num = 0;
+	//int i = 0, j = 0;
+	//while (scanf_s("%d", &num) != EOF)
+	//{
+	//	for (i = 0; i < num; i++)
+	//	{
+	//		for (j = 0; j < num - 1 - i; j++)
+	//		{
+	//			printf(" ");
+	//		}
+	//		for (j = 0; j < i; j++)
+	//		{
+	//			printf("* ");
+	//		}
+	//		printf("* ");
+	//		printf("\n");
+	//	}
+	//	
+	//}
+
+	//BC62
+	//int num = 0;
+	//int i = 0, j = 0;
+	//while (scanf_s("%d", &num) !=EOF)
+	//{
+	//	for (i = 0; i < num; i++)
+	//	{
+	//		for (j = 0; j < i; j++)
+	//		{
+	//			printf(" ");
+	//		}
+	//		for (j = 0; j < num - 1 - i; j++)
+	//		{
+	//			printf("* ");
+	//		}
+	//		printf("* ");
+	//		printf("\n");
+	//	}
+	//}
+
+	//BC63
+	//int num = 0;
+	//int i = 0, j = 0;
+	//while (scanf_s("%d", &num) != EOF)
+	//{
+	//	for (i = 0; i < num; i++)
+	//	{
+	//		for (j = 0; j < num - i; j++)
+	//		{
+	//			printf(" ");
+	//		}
+	//		for (j = 0; j < i; j++)
+	//		{
+	//			printf("* ");
+	//		}
+	//		printf("* ");
+	//		printf("\n");
+	//	}
+	//	num = num + 1;
+	//	for (i = 0; i < num; i++)
+	//	{
+	//		for (j = 0; j < i; j++)
+	//		{
+	//			printf(" ");
+	//		}
+	//		for (j = 0; j < num - 1 - i; j++)
+	//		{
+	//			printf("* ");
+	//		}
+	//		printf("* ");
+	//		printf("\n");
+	//	}
+	//}
+
+	return 0;
+}
