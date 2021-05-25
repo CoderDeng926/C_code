85e935c5b5dc7de098eedf4232c62a0aaf75d163
CoderDeng926 <djq2926@icloud.com>
2021-05-25 21:47:41 +0800

    21年5月25日 牛客网 编程初学者入门训练 摸鱼

---
+++ Project_21_5_25_水了/practice.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#define MAXLENGTH 100
+
+int main()
+{
+	//牛客网 编程初学者入门训练
+	//DAY12
+
+	//BC96
+	//int N = 0;
+	//int arr[100] = { 0 };
+	//int i = 0;
+	//int count1 = 0, count2 = 0;
+	//scanf_s("%d", &N);
+	//for (i = 0; i < N; i++)
+	//{
+	//	scanf_s("%d ", &arr[i]);
+	//}
+	//int j = 0;
+	//for (j = 0; j < N; j++)
+	//{
+	//	if (arr[j] > arr[j + 1] && arr[j - 1] > arr[j])
+	//		count1++;
+	//	else if (arr[j] < arr[j + 1] && arr[j - 1] < arr[j])
+	//		count2++;
+	//}
+	//if (count1 == N - 2 || count2 == N - 2)
+	//
+	//	printf("sorted\n");
+	//
+	//else  printf("unsorted\n"); 
+
+	//BC97
+	//int N = 0;
+	//int arr[100] = { 0 };
+	//scanf_s("%d", &N);
+	//int i = 0;
+	//for (i = 0; i < N; i++)
+	//{
+	//	scanf_s("%d ", &arr[i]);
+	//}
+	//int add = 0;
+	//scanf_s("%d", &add);
+	//int j = 0;
+	///*for (j = 0; j < N; j++)
+	//{*/
+	//	for (int k = 0; k < N + 1 - j; k++)
+	//	{
+	//		if (arr[k] > arr[k + 1])
+	//		{
+	//			int temp = arr[k];
+	//			arr[k] = arr[k + 1];
+	//			arr[k + 1] = temp;
+	//		}
+	//	}
+	///*}*/
+	//for (i = 0; i < N + 1; i++)
+	//{
+	//	printf("%d ", &arr[i]);
+	//}
+
+	return 0;
+}
